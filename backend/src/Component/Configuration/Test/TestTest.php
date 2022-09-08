a6dd8d11c49fdc2a5fd02f1a3687c70f7ecc5a4c
Alexey Shmelev <alexsey_89@bk.ru>
2022-09-08 22:55:13 +0300

    added test

---
+++ backend/src/Component/Configuration/Test/TestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaService\Component\Configuration\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
+{
+    public function testConf()
+    {
+        self::assertCount(2,[1,2]);
+    }
+}
